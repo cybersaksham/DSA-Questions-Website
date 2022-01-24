53d6c0f5117602d6fbbc4f92e67ba29b6d2e88b0
cybersaksham <2020UCP1801@mnit.ac.in>
2022-01-24 10:41:01 +0530

    ids

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pandas as pd
 import json
+import ids
 
 final = json.load(open("final.json"))
+final_ids = ids.ids
 
 for item in final:
     final[item] = []
 
+count = 0
+
 
 def get_final(x):
+    global count
     if x["Topic"]:
         if x["Link"]:
-            lst = {"question": x["Problem"], "link": x["Link"]}
+            lst = {"id": final_ids[count], "question": x["Problem"], "link": x["Link"]}
             final[x["Topic"]].append(lst)
+            count += 1
 
 
 if __name__ == '__main__':
